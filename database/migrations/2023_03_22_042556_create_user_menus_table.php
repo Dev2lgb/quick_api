8463fa9bac0efcbff0a69c519f4ecac1b1021dfa
LeeGyuBong <dev2lgb@gmail.com>
2023-03-29 14:37:48 +0900

    20230329

---
+++ database/migrations/2023_03_22_042556_create_user_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->comment('메뉴명');
+            $table->string('url')->comment('경로');
+            $table->integer('order')->comment('정렬순서');
+            $table->bigInteger('content_id')->comment('콘텐츠키');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_menus');
+    }
+};
---
+++ database/migrations/2023_03_22_045224_create_main_visuals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('main_visuals', function (Blueprint $table) {
+            $table->id();
+            $table->text('title')->comment('메인텍스트');
+            $table->text('subtitle')->comment('서브텍스트');
+            $table->integer('order')->comment('정렬순서');
+            $table->enum('isUse', ['Y','N'])->comment('사용여부');
+            $table->integer('order')->comment('정렬순서');
+            $table->string('fileName', 255)->comment('파일명');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('main_visuals');
+    }
+};
