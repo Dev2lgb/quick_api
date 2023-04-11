4e6dfb436d607d422af2f86bd5ccab34541b922c
backhorock <our-web@our-web.co.kr>
2023-04-11 12:49:14 +0900

    22

--- app/Http/Controllers/DesignSettingController.php
+++ app/Http/Controllers/DesignSettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\DesignSetting;
+use Illuminate\Support\Facades\Log;
 
 class DesignSettingController extends Controller
 {
@@ -13,8 +14,59 @@ class DesignSettingController extends Controller
             $inputData = $request->all();
             //  $inputData['추가되는 값'] = '추가되는 값을 입력함';
             $targetData = DesignSetting::create($inputData);
-        }catch(\Exception $e){
+            $code = 200;
+            $status = 'succcess';
+            $message = '등록에 성공하였습니다.';
 
+            return response()->json([
+                'data' => $targetData,
+                'status' => $status,
+                'message' => $message,
+            ], $code);
+
+        }catch(\Exception $e){
+            $code = 200;
+            $status = 'fail';
+            $message = '등록에 실패하였습니다.';
+
+            $error = sprintf('[%s],[%d] ERROR:[%s]', __METHOD__, __LINE__, json_encode($e->getMessage(), true));
+            Log::error($error);
+
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'error' => $error,
+            ], $code);
+        }
+    }
+
+    //  index : list 추출
+    public function index(Request $request){
+        try {
+            $targetData = DesignSetting::paginate(15);
+            $code = 200;
+            $status = 'succcess';
+            $message = '리스트 추출에 성공하였습니다.';
+
+            return response()->json([
+                'data' => $targetData,
+                'status' => $status,
+                'message' => $message,
+            ], $code);
+
+        }catch(\Exception $e){
+            $code = 200;
+            $status = 'fail';
+            $message = '리스트 추출에 실패하였습니다.';
+
+            $error = sprintf('[%s],[%d] ERROR:[%s]', __METHOD__, __LINE__, json_encode($e->getMessage(), true));
+            Log::error($error);
+
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'error' => $error,
+            ], $code);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 */
 
 Route::post('/designsetting',[DesignSettingController::class,'store']);
+Route::get('/designsetting',[DesignSettingController::class,'index']);
